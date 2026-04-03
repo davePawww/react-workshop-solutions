1be83503376995457421baaa62d1a9946d47fdd6
DavePaurillo <paurillo.dave@gmail.com>
2026-04-03 22:21:31 +0800

    chore: shadcn setup

--- 01-todo-list/src/App.tsx
+++ 01-todo-list/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 function App() {
   return (
     <>
       <p className="text-3xl font-bold text-red-500 underline">Test</p>
+      <Button className="rounded">Test</Button>
     </>
   );
 }
