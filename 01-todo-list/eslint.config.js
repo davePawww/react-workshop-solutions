2bd7992a46d800314e2a70d8c1356b6e3f961f26
DavePaurillo <paurillo.dave@gmail.com>
2026-04-04 01:00:16 +0800

    chore: eslint-plugin-unused-imports setup

--- 01-todo-list/eslint.config.js
+++ 01-todo-list/eslint.config.js
@@ -7,12 +7,13 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 import eslintConfigPrettier from 'eslint-config-prettier'
 import react from 'eslint-plugin-react'
 import importPlugin from 'eslint-plugin-import'
+import unusedImports from 'eslint-plugin-unused-imports'
 
 export default defineConfig([
   globalIgnores(['dist', 'node_modules', 'coverage']),
   {
     files: ['**/*.{ts,tsx}'],
-    plugins: { import: importPlugin },
+    plugins: { import: importPlugin, 'unused-imports': unusedImports },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommendedTypeChecked,
@@ -42,6 +43,19 @@ export default defineConfig([
           groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'type'],
           'newlines-between': 'always',
           alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+      // eslint plugin unused imports
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
         },
       ],
     },
