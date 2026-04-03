f3e746179c3200014a4184d1bd72555f236034e6
DavePaurillo <paurillo.dave@gmail.com>
2026-04-03 22:52:02 +0800

    chore: vite react-ts scaffolding

--- 01-todo-list/eslint.config.js
+++ 01-todo-list/eslint.config.js
@@ -1,76 +1,23 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import { defineConfig, globalIgnores } from 'eslint/config';
-import prettierConfig from 'eslint-config-prettier';
-import react from 'eslint-plugin-react';
-import importPlugin from 'eslint-plugin-import';
-import jsxA11y from 'eslint-plugin-jsx-a11y';
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    plugins: {
-      import: importPlugin,
-    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-      react.configs.flat.recommended,
-      jsxA11y.flatConfigs.recommended,
-      prettierConfig,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    settings: {
-      react: { version: 'detect' },
-      'import/resolver': { typescript: true },
-    },
-    rules: {
-      // eslint-plugin-react
-      'react/react-in-jsx-scope': 'off',
-      // eslint-plugin-import
-      'import/no-duplicates': 'error',
-      'import/no-cycle': 'warn',
-      'import/order': [
-        'error',
-        {
-          groups: [
-            'builtin',
-            'external',
-            'internal',
-            ['parent', 'sibling', 'index'],
-          ],
-          'newlines-between': 'always',
-          alphabetize: { order: 'asc' },
-        },
-      ],
-      // typescript
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        { argsIgnorePattern: '^_' },
-      ],
-      '@typescript-eslint/consistent-type-imports': 'error',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      'no-console': 'warn',
-      eqeqeq: 'error',
-      'no-debugger': 'error',
-    },
   },
-  // For Tests
-  {
-    files: ['**/*.test.{ts,tsx}', '**/*.spec.{ts,tsx}', '**/__tests__/**'],
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      'no-console': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-    },
-  },
-]);
+])
