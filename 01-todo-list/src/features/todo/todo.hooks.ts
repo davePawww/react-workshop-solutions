edcccb69fd4e10b6ff0ae7881b9ae868dc4fd72d
DavePaurillo <paurillo.dave@gmail.com>
2026-04-05 00:27:59 +0800

    refactor: added memoization

--- 01-todo-list/src/features/todo/todo.hooks.ts
+++ 01-todo-list/src/features/todo/todo.hooks.ts
@@ -1,3 +1,5 @@
+import { useCallback } from 'react'
+
 import { useLocalStorage } from '@/hooks/useLocalStorage'
 
 import type { Todo } from '@/features/todo/todo.types'
@@ -5,34 +7,43 @@ import type { Todo } from '@/features/todo/todo.types'
 export function useTodoList() {
   const [todos, setTodos] = useLocalStorage<Todo[]>('todos', [])
 
-  const addTodo = (text: string) => {
-    const newTodo = {
-      id: Date.now(),
-      text,
-      completed: false,
-    }
+  const addTodo = useCallback(
+    (text: string) => {
+      const newTodo = {
+        id: Date.now(),
+        text,
+        completed: false,
+      }
 
-    setTodos((prev) => [newTodo, ...prev])
-  }
+      setTodos((prev) => [newTodo, ...prev])
+    },
+    [setTodos],
+  )
 
-  const toggleTodo = (id: number) => {
-    setTodos((prev) =>
-      prev.map((todo) => {
-        if (id === todo.id) {
-          return {
-            ...todo,
-            completed: !todo.completed,
+  const toggleTodo = useCallback(
+    (id: number) => {
+      setTodos((prev) =>
+        prev.map((todo) => {
+          if (id === todo.id) {
+            return {
+              ...todo,
+              completed: !todo.completed,
+            }
           }
-        }
 
-        return todo
-      }),
-    )
-  }
+          return todo
+        }),
+      )
+    },
+    [setTodos],
+  )
 
-  const deleteTodo = (id: number) => {
-    setTodos((prev) => prev.filter((todo) => todo.id !== id))
-  }
+  const deleteTodo = useCallback(
+    (id: number) => {
+      setTodos((prev) => prev.filter((todo) => todo.id !== id))
+    },
+    [setTodos],
+  )
 
   return { todos, addTodo, toggleTodo, deleteTodo }
 }
