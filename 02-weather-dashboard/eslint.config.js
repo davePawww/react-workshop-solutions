09a7efe85f7263941bbd163dc90a6a8b450b8a13
DavePaurillo <paurillo.dave@gmail.com>
2026-04-07 18:02:19 +0800

    chore: eslint-plugin-import setup

--- 02-weather-dashboard/eslint.config.js
+++ 02-weather-dashboard/eslint.config.js
@@ -6,11 +6,13 @@ import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import react from 'eslint-plugin-react';
+import importPlugin from 'eslint-plugin-import';
 
 export default defineConfig([
   globalIgnores(['dist', 'node_modules', 'coverage']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: { import: importPlugin },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommendedTypeChecked,
@@ -32,11 +34,37 @@ export default defineConfig([
       'react/jsx-no-useless-fragment': 'error',
       'react/self-closing-comp': 'error',
       'react/prop-types': 'off',
+      // eslint plugin import
+      'import/no-unresolved': 'error',
+      'import/no-duplicates': 'error',
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'type',
+          ],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
     },
     settings: {
       // eslint plugin react
       react: {
         version: 'detect',
+      },
+      // eslint plugin import
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+        },
+        node: true,
       },
     },
   },
