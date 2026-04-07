31ce2d7b71541c5966db7e78563137ba9b9dbec5
DavePaurillo <paurillo.dave@gmail.com>
2026-04-07 17:56:02 +0800

    chore: eslint-plugin-react setup | prettierignore

--- 02-weather-dashboard/eslint.config.js
+++ 02-weather-dashboard/eslint.config.js
@@ -5,20 +5,39 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import react from 'eslint-plugin-react';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules', 'coverage']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      react.configs.flat.recommended,
+      react.configs.flat['jsx-runtime'],
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 2023,
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      // eslint plugin react
+      'react/jsx-no-useless-fragment': 'error',
+      'react/self-closing-comp': 'error',
+      'react/prop-types': 'off',
+    },
+    settings: {
+      // eslint plugin react
+      react: {
+        version: 'detect',
+      },
     },
   },
   eslintConfigPrettier,
